ecf3e93b335457f57712482542f232c34d60a303
Vinícius Dias Rodrigues <107416723+Viniddev@users.noreply.github.com>
2024-03-21 13:37:38 -0300

    My challenge update

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace program {
+    public class  Program
+    {
+        public static void Main(string[] args) 
+        {
+            Iphone iphone = new Iphone("31 99922-6468", "2", "2", 64);
+            Nokia nokia = new Nokia("31 99922-9999", "3", "3", 32);
+
+            Console.WriteLine("--------------");
+            Console.WriteLine(nokia.ToString());
+            Console.WriteLine("--------------");
+            Console.WriteLine(iphone.ToString());
+            Console.WriteLine("--------------");
+        }
+    }
+}
